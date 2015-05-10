d6b5162983292b8f27e3ed618a255056e10370d7
Violeta Feliciano Sanjuan <violetaonil@hotmail.com>
2015-05-10 17:31:41 +0200

    Suite JUnit con todos los Tests

---
+++ TodosLosTests.java
@@ -0,0 +1,17 @@
+package unit9.unit9;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ acabaEnTest.class, ComparaIgnorandoMayusculasTest.class,
+		concatenaCadenasTest.class, convertirMayusculasTest.class,
+		convertirMinusculasTest.class, empiezaConTest.class, esMayorTest.class,
+		esMenorTest.class, ExtraerSubstringTest.class,
+		longitudCadenaTest.class, posicionPrimeraCadenaTest.class,
+		QuitaEspaciosTest.class, reemplazaPrimeroTest.class,
+		reemplazaTodosTest.class, SonIgualesTest.class })
+public class TodosLosTests {
+
+}
